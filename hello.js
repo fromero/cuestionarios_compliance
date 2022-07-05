fcc515d29c7844ffc3c66e6c402219d924bc431a
fromero <francisco.romero.orcajada@gmail.com>
2022-07-05 22:00:37 +0200

    z

--- hello.js
+++ hello.js
@@ -1,6 +1,7 @@
 import { serve } from "https://deno.land/std@0.140.0/http/server.ts";
 
 serve((_req) => {
+  console.log(_req);
   const payload = {hello: "World"}
   return new Response(JSON.stringify(payload), {
     headers: { "content-type": "application/json" },
