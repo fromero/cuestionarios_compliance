859f9b81c2ce0c8f60c94a246bce866ceedf3230
fromero <francisco.romero.orcajada@gmail.com>
2022-07-05 22:08:26 +0200

    m

--- hello.js
+++ hello.js
@@ -1,9 +1,17 @@
 import { serve } from "https://deno.land/std@0.140.0/http/server.ts";
 
 serve((_req) => {
-  console.log(_req);
-  const payload = {hello: "World"}
-  return new Response(JSON.stringify(payload), {
+  const body = `{"name": "Deno"}`;
+  resp = await fetch("https://173a0n56qe.execute-api.eu-west-1.amazonaws.com/newjwt", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": "Bearer e47bb63caae08e0dea4346f701bbcbf52ab717ad70f11f0710008c9bbe50431d",
+    },
+    body,
+  });
+  
+  return new Response(JSON.stringify(resp.body), {
     headers: { "content-type": "application/json" },
   });
 });
